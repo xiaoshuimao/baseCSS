1c88afdb9569676e12337a9763f831f5aae78965
i in yun <lizhygm@sina.com>
2015-06-05 13:57:43 +0800

    create grunt & _init & normalize

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      dist: 'dist'
+    },
+    less: {
+      compileCore: {
+        options: {
+          compress: false
+        },
+        src: 'less/<%= pkg.name %>.less',
+        dest: 'dist/css/<%= pkg.name %>.css'
+      },
+      compileTheme: {
+        options: {
+          compress: false
+        },
+        src: 'less/<%= pkg.name %>-theme.less',
+        dest: 'dist/css/<%= pkg.name %>-theme.css'
+      }
+    },
+    cssmin: {
+      ptions: {
+        compatibility: 'ie8',
+        keepSpecialComments: '*',
+        advanced: false
+      },
+      minifyCore: {
+        src: 'dist/css/<%= pkg.name %>.css',
+        dest: 'dist/css/<%= pkg.name %>.min.css'
+      },
+      minifyTheme: {
+        src: 'dist/css/<%= pkg.name %>-theme.css',
+        dest: 'dist/css/<%= pkg.name %>-theme.min.css'
+      }
+    },
+    watch: {
+      options: {
+        atBegin: true
+      },
+      css: {
+        files: [
+          'less/*.less'
+        ],
+        tasks: [
+          'less'
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('pro', ['less', 'cssmin']);
+}
